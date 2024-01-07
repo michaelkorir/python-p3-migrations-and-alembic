90a36bb5f952c32bda42f54749ec1380102c97be
michael-korir <michaelkiptoo2000@gmail.com>
2024-01-07 14:31:20 +0300

    done with migrations

---
+++ lib/migrations/versions/b9a32afbc3db_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b9a32afbc3db
+Revises: 
+Create Date: 2024-01-07 13:38:12.142278
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b9a32afbc3db'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
